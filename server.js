a301f34f5bf9401fb5c9a0655e041cc1c6a942fc
RenataCunha0 <renatapatriciacunha@gmail.com>
2020-05-11 22:55:28 +0000

    Loja de roupa pronto

--- server.js
+++ server.js
@@ -2,8 +2,57 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', function(request, response) {
-    response.send('Hello World!');
+server.use(express.json());
+
+// lojaderoupa(id,cliente,produto, fornecedor)
+
+const lojaderoupa = [
+    {id: 1, cliente: 'Cristina', produto: 'Blusa de Frio', fornecedor: 'Prada'},
+    {id: 2, cliente: 'Angela', produto: 'Vestido', fornecedor:'Dolce e Gabbana'} 
+]
+
+server.get('/lojaderoupa', function(request, response) {
+    response.json(lojaderoupa);
 })
+ 
+server.post('/lojaderoupa', function(request, response) {
+   
+    const {id, cliente, produto, fornecedor} = request.body;
+
+    lojaderoupa.push({id, cliente, produto, fornecedor})
+    response.status(204).send();
+})
+
+server.put('/lojaderoupa/:id', function(request, response) {
+    const id = request.params.id;
+    const {cliente, produto, fornecedor} = request.body;
+
+   for(let i = 0; i < lojaderoupa.length; i++){
+        if(lojaderoupa[i].id == id) {
+            lojaderoupa[i].cliente = cliente;
+            lojaderoupa[i].produto = produto;
+            lojaderoupa[i].fornecedor = fornecedor;
+            break;
+        }
+    }
+
+    return response.status(204).send();
+
+})
+
+server.delete('/lojaderoupa/:id', function(request, response) {
+
+    const id = request.params.id;
+
+    for(let i = 0; i < lojaderoupa.length; i++){
+        if(lojaderoupa[i].id == id) {
+            lojaderoupa.splice(i, 1)
+            break;
+        }
+    }
+
+    return response.status(204).send();
+})
+
 
 server.listen(process.env.PORT || 3000);
